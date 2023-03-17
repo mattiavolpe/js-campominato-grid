9c83cec485ae37743e05aabb2c0dfc240c3398e8
mattiavolpe <64392380+MatthewFox90@users.noreply.github.com>
2023-03-17 17:10:20 +0100

    ADD Grid size decision and Play button behavior

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,45 @@
+"use strict";
+
 const containerElement = document.querySelector(".container");
 
-for (let i = 1; i <= 100; i++) {
-  const cellElement = document.createElement("div");
-  cellElement.classList.add("cell");
-  cellElement.innerHTML = i;
-  containerElement.insertAdjacentElement("beforeend", cellElement);
-}
+let cellsNumber = 49;
+
+const levelSelectorElement = document.querySelector("#level");
+
+levelSelectorElement.addEventListener("change", function() {
+  switch (this.value) {
+    case "medium":
+      cellsNumber = 81;
+      break;
+    case "hard":
+      cellsNumber = 100;
+      break;
+    default:
+      cellsNumber = 49;
+  }
+});
+
+const playButton = document.querySelector("#game_options > button");
+
+playButton.addEventListener("click", function () {
+  containerElement.innerHTML = "";
+  const cellsPerRow = Math.sqrt(cellsNumber);
+  for (let i = 1; i <= cellsNumber; i++) {
+    const cellElement = document.createElement("div");
+    cellElement.classList.add("cell");
+    cellElement.style.height = `min(calc(100% / ${cellsPerRow}), (96vw / ${cellsPerRow}))`;
+    cellElement.innerHTML = i;
+    containerElement.insertAdjacentElement("beforeend", cellElement);
+  }
+})
 
 const insertedCells = document.querySelectorAll(".cell");
+
 for (let i = 0; i < insertedCells.length; i++) {
-  currentCell = insertedCells[i];
+  const currentCell = insertedCells[i];
   currentCell.addEventListener("click", function() {
     this.classList.add("selected");
     console.log(Number(this.innerText));
   });
 }
+
